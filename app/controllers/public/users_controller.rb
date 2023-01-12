e7a414ea5094cc9da844fc8eb5e5ae5e073a6d67
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2023-01-12 13:37:44 +0000

    ユーザー画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,30 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user= User.find(params[:id])
+    @posts= @user.posts
   end
 
   def edit
+    @user= User.find(params[:id])
   end
 
   def update
+    @user= User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:family_name, :first_name, :profile_image, :nickname, :email, :introduction)
   end
 
 end
