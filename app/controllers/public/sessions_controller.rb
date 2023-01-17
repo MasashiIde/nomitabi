f96a662ecb2d7699136105cadda479246f732f53
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2023-01-17 11:00:17 +0000

    管理者機能追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,8 +3,6 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  before_action :reject_inactive_user, only: [:create]
-
   # GET /resource/sign_in
   # def new
   #   super
@@ -26,18 +24,6 @@ class Public::SessionsController < Devise::SessionsController
   # end
   def after_sign_in_path_for(resource)
     root_path
-  end
-
-  protected
-
-  #会員の論理削除のための記述
-  def reject_inactive_user
-    @user = User.find_by(email: params[:user][:email])
-    if @user
-      if @user.valid_password?(params[:user][:password]) && !@user.is_deleted
-        redirect_to new_user_session_path
-      end
-    end
   end
 
 end
