4e30dc83eb8e69d558e8eda219217b54a936a46b
MasashiIde <nanairo46mabo@gmail.com>
2023-01-22 10:39:19 +0000

    migrateファイル修正

--- db/migrate/20230111061639_create_posts.rb
+++ db/migrate/20230111061639_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
 
       t.integer :user_id,      null: false
       t.string  :shop_name,    null: false, default: ""
-      t.text    :introduction, null: false, default: ""
+      t.text    :introduction, null: false
       t.string  :shop_address, null: false, default: ""
       t.string  :shop_url
 
